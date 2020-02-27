af7c71d32b4e100f0809920b44893f1048c12e89
senda488 <60650701+senda488@users.noreply.github.com>
2020-02-27 10:18:22 +0900

    tweet基本型

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+  before_action :post_edit_show, only:[:edit, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
+
+  def index
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+    # @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end  
+
+  def edit
+   
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @post.comments.includes(:user)
+  end
+
+  def search
+    @posts = Post.search(params[:keyword])
+  end  
+
+  private
+  def post_params
+    params.require(:post).permit(:text, :image).merge(user_id: current_user.id)
+  end 
+
+  def post_edit_show
+    @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+
+  def self.search(search)
+    return Post.all unless search
+    Post.where('text LIKE(?)', "%#{search}%")
+  end
 end
